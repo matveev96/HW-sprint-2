3ebcfcfb3c546f2c5793978491e9856594793b17
matveev96 <sasha.matveev1996@icloud.com>
2024-12-21 22:24:11 +0300

    Sprint 2 week 3 HW3 is Done

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react'
 import HW1 from '../s2-homeworks/hw01/HW1'
-import styled from "styled-components";
 import HW2 from "../s2-homeworks/hw02/HW2";
+import HW3 from "../s2-homeworks/hw03/HW3";
 
 function App() {
     return (
-        <AppStyle>
+        <div>
             <HW1/>
             {/*раскомментировать по ходу выполнения*/}
             <HW2/>
-            {/*<HW3/>*/}
+            <HW3/>
             {/*<HW4/>*/}
 
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
             {/*<HW5/>*/}
-        </AppStyle>
+        </div>
     )
 }
 
 export default App
-
-
-const AppStyle = styled.div`
-
-`
--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -1,15 +1,16 @@
 import React, {ChangeEvent, KeyboardEvent} from 'react'
 import s from './Greeting.module.css'
+import styled from "styled-components";
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    onBlur: any // need to fix any
-    onEnter: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
-    lastUserName?: any // need to fix any
+    name: string // need to fix any
+    setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void // need to fix any
+    addUser: () => void // need to fix any
+    onBlur: () => void // need to fix any
+    onEnter: (e: KeyboardEvent<HTMLInputElement>) => void // need to fix any
+    error: string // need to fix any
+    totalUsers: number // need to fix any
+    lastUserName?: string // need to fix any
 }
 
 // презентационная компонента (для верстальщика)
@@ -25,20 +26,20 @@ const Greeting: React.FC<GreetingPropsType> = (
         lastUserName,
     } // деструктуризация пропсов
 ) => {
-    const inputClass = s.errorInput // need to fix with (?:)
+    const inputClass = error ? s.errorInput : "" // need to fix with (?:)
 
     return (
-        <div id={'hw3-form'} className={s.greetingForm}>
-            <div className={s.text}>
+        <GreetingWrapper id={'hw3-form'} className={s.greetingForm}>
+            <PeopleAdd className={s.text}>
                 {'Людей добавили: '}
-                <span id={'hw3-users-total'}>
+                <TotalUsers id={'hw3-users-total'}>
                     {totalUsers}
-                </span>
-            </div>
+                </TotalUsers>
+            </PeopleAdd>
 
-            <div className={s.inputAndButtonContainer}>
-                <div>
-                    <input
+            <InputAndButtonContainer className={s.inputAndButtonContainer}>
+                <InputContainer>
+                    <Input
                         id={'hw3-input'}
                         value={name}
                         onChange={setNameCallback}
@@ -46,28 +47,90 @@ const Greeting: React.FC<GreetingPropsType> = (
                         onKeyDown={onEnter}
                         onBlur={onBlur}
                     />
-                    <div id={'hw3-error'} className={s.error}>
+                    <Error id={'hw3-error'} className={s.error}>
                         {error}
-                    </div>
-                </div>
+                    </Error>
+                </InputContainer>
 
-                <button
+                <Button
                     id={'hw3-button'}
                     onClick={addUser}
                     className={s.button}
                     disabled={!name.trim()}
                 >
-                    add
-                </button>
-            </div>
+                    Add
+                </Button>
+            </InputAndButtonContainer>
 
             {lastUserName && (
-                <div className={s.greeting}>
+                <GreetingName className={s.greeting}>
                     Привет <span id={'hw3-last-user'}>{lastUserName}</span>!
-                </div>
+                </GreetingName>
             )}
-        </div>
+        </GreetingWrapper>
     )
 }
 
 export default Greeting
+
+const GreetingWrapper = styled.div`
+`
+
+const PeopleAdd = styled.div`
+    font-size: 14px;
+    font-family: 'Montserrat', sans-serif;
+    opacity: .5;
+`
+
+const TotalUsers = styled.span`
+`
+const InputAndButtonContainer = styled.div`
+    display: flex;
+    gap: 12px;
+    margin-top: 9px;
+`
+
+const InputContainer = styled.div`
+    
+`
+const Error = styled.div`
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+    color: #c00;
+`
+
+const Input = styled.input<{className: string}>`
+    margin-bottom: 7px ;
+    padding-left: 12px;
+    border-radius: 5px;
+    border: 1px solid ${props => props.className ? "#c00" : "#d1d1d1"};
+    width: 372px;
+    height: 36px;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 500;
+    font-size: 16px;
+    color: #000;
+`
+
+const Button = styled.button<{disabled: boolean}>`
+    width: 78px;
+    height: 36px;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 1.4;
+    color: #fff;
+    border: none;
+    background: #06c;
+    opacity: ${props => props.disabled ? 0.5 : 1};
+    border-radius: 3px;
+    cursor: pointer;
+`
+
+const GreetingName = styled.div`
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 500;
+    font-size: 14px;
+    color: #000;
+`
