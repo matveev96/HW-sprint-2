78106d1013d6be7e97a531d96e3f3d7ffbb27e30
matveev96 <sasha.matveev1996@icloud.com>
2025-02-16 21:58:37 +0300

    feat: HW11 is done

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
-import { restoreState } from '../hw06/localStorage/localStorage'
+import {restoreState} from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
+import {S} from '../../s1-main/AppStyles'
 
 /*
 * 1 - передать значения в оба слайдера
@@ -15,22 +16,29 @@ function HW11() {
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = (event: any, value: any) => {
+    const change = (event: Event, value: number | number[]) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+        if (typeof value === 'number') {
+            setValue1(value)
+        } else {
+            setValue1(value[0])
+            setValue2(value[1])
+        }
     }
 
     return (
-        <div id={'hw11'}>
-            <div className={s2.hwTitle}>Homework #11</div>
+        <S.HwWrapper id={'hw11'}>
+            <S.Title className={s2.hwTitle}>Hometask №11</S.Title>
 
-            <div className={s2.hw}>
+            <S.ContentWrapper className={s2.hw}>
                 <div className={s.container}>
                     <div className={s.wrapper}>
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-single-slider'}
                             // сделать так чтоб value1 изменялось // пишет студент
-
+                            onChange={change}
+                            value={value1}
                         />
                     </div>
                     <div className={s.wrapper}>
@@ -38,13 +46,14 @@ function HW11() {
                         <SuperRange
                             id={'hw11-double-slider'}
                             // сделать так чтоб value1/2 изменялось // пишет студент
-
+                            onChange={change}
+                            value={[value1, value2]}
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
                 </div>
-            </div>
-        </div>
+            </S.ContentWrapper>
+        </S.HwWrapper>
     )
 }
 
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,15 +1,53 @@
 import React from 'react'
 import {Slider, SliderProps} from '@mui/material'
+import styled from "styled-components";
 
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
+        // <Slider
+        //     sx={{ // стили для слайдера // пишет студент
+        //         color: '#00CC22',
+        //         width: '147px',
+        //         height: '4px'
+        //     }}
+        //     {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+        // />
+        <StyledSlider
+            {...props}
+            sx={{
+                width: '147px',
+                height: '4px'
             }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
     )
 }
 
 export default SuperRange
+
+
+const StyledSlider = styled(Slider)({
+    '& .MuiSlider-track': {
+        color: '#00CC22',
+    },
+    '& .MuiSlider-thumb': {
+        height: 18,
+        width: 18,
+        backgroundColor: '#fff',
+        border: '2px solid #00CC22',
+        '&:before': {
+            content: '""',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            width: 5,
+            height: 5,
+            borderRadius: '50%',
+            backgroundColor: '#00CC22',
+            transform: 'translate(-50%, -50%)',
+        },
+    },
+    '& .MuiSlider-rail': {
+        backgroundColor: '#8b8b8b',
+    },
+
+})
