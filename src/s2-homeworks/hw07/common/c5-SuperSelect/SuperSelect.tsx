0cad58245c7ec13bca194bc64ecdd5334df92ede
matveev96 <sasha.matveev1996@icloud.com>
2025-03-16 10:15:15 +0300

    feat: add onChange in onChangeCallback

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -40,6 +40,9 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
         if (onChangeOption) {
             onChangeOption(e.currentTarget.value)
         }
+        if (onChange) {
+            onChange(e)
+        }
     }
 
     const finalSelectClassName = s.select + (className ? ' ' + className : '')
